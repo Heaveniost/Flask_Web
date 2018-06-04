fdaedb40ee8e9509166604a1d3591706db2b621f
Heaveniost <304090717@qq.com>
2018-06-04 22:28:59 +0800

    Custom error pages

--- hello.py
+++ hello.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 manager = Manager(app)
 Bootstrap = Bootstrap(app)
 
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'),500
+
 @app.route('/')
 def index():
 	return render_template('index.html')
