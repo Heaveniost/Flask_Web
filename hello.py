3ae44c52531211253091070277e0b4d9fb6eaec7
Heaveniost <304090717@qq.com>
2018-06-05 21:44:34 +0800

    Database use in the application

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask,render_template,session,redirect,url_for,flash
+from flask import Flask,render_template,session,redirect,url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap 
 from flask_moment import Moment
@@ -25,7 +25,7 @@ db = SQLAlchemy(app)
 
 class Role(db.Model):
 	__tablename__ = 'roles'
-	id = db.Column(db.Interger,primary_key=True)
+	id = db.Column(db.Integer,primary_key=True)
 	name = db.Column(db.String(64),unique=True)
 	users = db.relationship('User',backref='role',lazy='dynamic')
 
@@ -34,9 +34,9 @@ class Role(db.Model):
 
 class User(db.Model):
 	__tablename__ = 'users'
-	id = db.Column(db.Interger,primary_key=True)
+	id = db.Column(db.Integer,primary_key=True)
 	username = db.Column(db.String(64),unique=True,index=True)
-	role_id = db.Column(db.Interger,db.ForeignKey('roles.id'))
+	role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
 	def __repr__(self):
 		return '<User %r>' %self.username
@@ -58,14 +58,21 @@ def internal_server_error(e):
 def index():
 	form = NameForm()
 	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('Looks like you have changed your name!')
+		# old_name = session.get('name')
+		# if old_name is not None and old_name != form.name.data:
+		# 	flash('Looks like you have changed your name!')
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username=form.name.data)
+			db.session.add(user)
+			session['konwn'] = False
+		else:
+			session['konwn'] = True
 		session['name'] = form.name.data
 		return redirect(url_for('index'))
-	return render_template('index.html',form=form,name=session.get('name'))
+	return render_template('index.html',form=form,name=session.get('name'),konwn=session.get('konwn',False))
 
 
 if __name__ == '__main__':
-	db.create_all()
+	# db.create_all()
 	manager.run( )
