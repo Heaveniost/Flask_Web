d49af39604bf1ea4cc9733df9fb2f7c41885729e
Heaveniost <304090717@qq.com>
2018-06-10 03:59:49 +0800

    Password updates

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,7 +5,7 @@ from . import auth
 from .. import db
 from ..models import User 
 from ..email import send_email
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm, ChangePasswordForm
 
 
 
@@ -77,6 +77,24 @@ def resend_confirmation():
 	send_email(current_user.email, 'Confirm Your Account', 'auth/email/confirm', user=current_user, token=token)
 	flash('A new confirmation email has been sent to you by email.')
 	return redirect(url_for('main.index'))
+
+
+@auth.route('/change-password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+	form = ChangePasswordForm()
+	if form.validate_on_submit():
+		if current_user.verify_password(form.old_password.data):
+			if form.old_password.data != form.password.data:
+				current_user.password = form.password.data
+				db.session.add(current_user)
+				flash('Your password has been updated.')
+				return redirect(url_for('main.index'))
+			else:
+				flash('The new password is equal to the old password')
+		else:
+			flash('Invalid password.')
+	return render_template("auth/change_password.html", form=form)
 
 
 
