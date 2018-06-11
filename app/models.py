45982e1c233e9980a94f6605636fd2d75919a4d6
Heaveniost <304090717@qq.com>
2018-06-12 03:30:22 +0800

    Blog posts (11a)

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ from flask_login import UserMixin, AnonymousUserMixin
 class Permission:
     FOLLOW  = 0x01
     COMMENT = 0x02
-    WRITE_ARTICLES =0x04
+    WRITE_ARTICLES = 0x04
     MODERATE_COMMENTS = 0x08
     ADMINISTER = 0x80
 
@@ -57,6 +57,7 @@ class User(UserMixin, db.Model):
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     avatar_hash = db.Column(db.String(32))
+    posts = db.relationship('Post',backref='author', lazy='dynamic')
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -165,6 +166,38 @@ class AnonymousUser(AnonymousUserMixin):
     def is_administrator(self):
         return False
 
+login_manager.anonymous_user = AnonymousUser
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
