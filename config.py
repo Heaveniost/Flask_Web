5330a09554cf3e20932503fe6763f63bcf5dc1a1
Heaveniost <304090717@qq.com>
2018-06-12 15:59:53 +0800

    Blog post pagination (11d)

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ class Config:
 	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
 	FLASKY_MAIL_SENDER = 'Flasky Admin <304090717@qq.com>'
 	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+	FLASKY_POSTS_PER_PAGE = 20
 
 	@staticmethod
 	def init_app(app):
