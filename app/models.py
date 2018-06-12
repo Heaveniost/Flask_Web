f54b902970a2cdb258dd138437c9092c0ec06225
Heaveniost <304090717@qq.com>
2018-06-12 17:49:45 +0800

    Rich text server side handling with Markdown and Bleach (11f)

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ import hashlib
 from . import db, login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer 
+from markdown import markdown 
+import bleach
 from flask import current_app, request
 from flask_login import UserMixin, AnonymousUserMixin
 
@@ -202,6 +204,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
@@ -219,6 +222,13 @@ class Post(db.Model):
                      author=u)
             db.session.add(p)
             db.session.commit()
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'), tags=allowed_tags, strip=True))
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 
 
