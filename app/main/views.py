00c6693153c864bb5e0a4b047c48f5f04d0db136
Heaveniost <304090717@qq.com>
2018-06-14 23:05:20 +0800

    Source code profiling and correct a problem (16b)

--- app/main/views.py
+++ app/main/views.py
@@ -11,7 +11,7 @@ from ..decorators import admin_required, permission_required
 @main.after_app_request
 def after_request(response):
 	for query in get_debug_queries():
-		if query.duration >= current_app.config['FLASKY_SLOW_QUERY_TIME']:
+		if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']:
 			current.app.logger.warning('Slow query: %s\nParameters: %s\nDuration:%fs\nContext: %s\n' % (query.statement, query.parameters, query.duration, query.context))
 	return response
 
